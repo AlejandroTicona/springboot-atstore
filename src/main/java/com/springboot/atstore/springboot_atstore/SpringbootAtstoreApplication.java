83d35cf4ab6e24e068de243f93246492dc15ad98
Aletdev <alejandrortsinani@outlook.com>
2024-11-30 06:24:33 -0400

    Scaffolding inicial

---
+++ src/main/java/com/springboot/atstore/springboot_atstore/SpringbootAtstoreApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.atstore.springboot_atstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAtstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAtstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/atstore/springboot_atstore/SpringbootAtstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.atstore.springboot_atstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAtstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
